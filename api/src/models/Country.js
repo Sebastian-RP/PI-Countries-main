8d4dac2760a92476605e45d00241a293bb82ba88
BUFFON <55423389+Sebastian-RP@users.noreply.github.com>
2022-04-23 21:28:55 -0500

    modelos y sus asociaciones creadas con sequelize

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,41 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    id:{
+      type: DataTypes.STRING(3),
+      allowNull: false,
+      primaryKey: true,
+      validate: { 
+        isAlpha: true,
+      },
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    img_flag: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true,
+      }
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    subregion: {
+      type: DataTypes.STRING
+    },
+    area: {
+      type: DataTypes.REAL
+    },
+    population:{
+      type: DataTypes.INTEGER
+    }
   });
 };
